ff81991cf09328a5d475806130bb6550def41071
santhoshkanna021 <santhoshkannar802@gmail.com>
2025-08-06 12:51:44 +0530

    error clear

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist',
-  },
-})
+  plugins: [react(), tailwindcss()]
+});
